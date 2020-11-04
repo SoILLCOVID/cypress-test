58e24a7e4cd9e37793f4777cc57e71e731c77680
Toby Samples <toby.samples@gmail.com>
2020-11-04 09:45:50 -0600

    Initial Push

---
+++ cypress/integration/soillcovid/window.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+context('Window', () => {
+    beforeEach(() => {
+      cy.visit('https://soillcovid.github.io')
+    })
+  
+    it('cy.window() - get the global window object', () => {
+      
+      cy.window().should('have.property', 'top')
+    })
+  
+    it('cy.document() - get the document object', () => {
+      
+      cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+    })
+  
+    it('cy.title() - get the title', () => {
+      // https://on.cypress.io/title
+      cy.title().should('include', 'Kitchen Sink')
+    })
+  })
+  
